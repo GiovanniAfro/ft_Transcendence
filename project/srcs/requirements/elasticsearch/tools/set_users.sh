4d36351a6980dd9fa88f3cf7e9d9d649bf437011
kichkiro <kichkiro@student.42firenze.it>
2024-07-17 15:29:51 +0000

    finish to configure ELK

--- project/srcs/requirements/elasticsearch/tools/set_users.sh
+++ project/srcs/requirements/elasticsearch/tools/set_users.sh
@@ -12,20 +12,11 @@ declare -A users_passwords
 users_passwords=(
 	[logstash_internal]="${LOGSTASH_INTERNAL_PASSWORD:-}"
 	[kibana_system]="${KIBANA_SYSTEM_PASSWORD:-}"
-	[metricbeat_internal]="${METRICBEAT_INTERNAL_PASSWORD:-}"
-	[filebeat_internal]="${FILEBEAT_INTERNAL_PASSWORD:-}"
-	[heartbeat_internal]="${HEARTBEAT_INTERNAL_PASSWORD:-}"
-	[monitoring_internal]="${MONITORING_INTERNAL_PASSWORD:-}"
-	[beats_system]="${BEATS_SYSTEM_PASSWORD=:-}"
 )
 
 declare -A users_roles
 users_roles=(
 	[logstash_internal]='logstash_writer'
-	[metricbeat_internal]='metricbeat_writer'
-	[filebeat_internal]='filebeat_writer'
-	[heartbeat_internal]='heartbeat_writer'
-	[monitoring_internal]='remote_monitoring_collector'
 )
 
 # --------------------------------------------------------
@@ -34,9 +25,6 @@ users_roles=(
 declare -A roles_files
 roles_files=(
 	[logstash_writer]='logstash_writer.json'
-	[metricbeat_writer]='metricbeat_writer.json'
-	[filebeat_writer]='filebeat_writer.json'
-	[heartbeat_writer]='heartbeat_writer.json'
 )
 
 # --------------------------------------------------------
@@ -83,7 +71,7 @@ for role in "${!roles_files[@]}"; do
 	log "Role '$role'"
 
 	declare body_file
-	body_file="${BASH_SOURCE[0]%/*}/roles/${roles_files[$role]:-}"
+	body_file="${BASH_SOURCE[0]%/*}/${roles_files[$role]:-}"
 	if [[ ! -f "${body_file:-}" ]]; then
 		sublog "No role body found at '${body_file}', skipping"
 		continue
