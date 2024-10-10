1dec1ce0e5b227d6a6c6b8a933a3b9ec0ca0bb0d
btani <beatricetnii@gmail.com>
2024-10-10 18:03:30 +0000

    aggiunte robe di stile in homeviews

--- project/services/app/dev_python/conf/pong_project/pong_game/static/js/views/HomeView.js
+++ project/services/app/dev_python/conf/pong_project/pong_game/static/js/views/HomeView.js
@@ -1,22 +1,22 @@
 const HomeView = {
     render: function() {
         const app = document.getElementById('app');
-        app.innerHTML = `
-        <card>	
+        app.innerHTML = `	
         	<div class="main-body">
 				<div class="row justify-content-md-center">
             		<div class="col-md-4 mb-3">
-            		  <div class="card">
-            		    <div class="card-body">
-                        <h1 class="h3 mb-3 font-weight-normal" style="text-align:center;">
-                        Welcome to Pong Game! Proceed with registration to play.
+            		  <div class="box-light" style="backdrop-filter: blur(15px); padding: 20px; border-radius: 8px;">
+            		    <div class="home-title">
+                        <h1 class="h3 font-weight-normal" style="text-align:center;">
+                          <span style="font-size: xx-large; font-weight: bold; color: white;">Welcome to Pong Game!</span><br>
+                          <span style="font-size: medium; font-weight: normal; color: white;">Proceed with registration to play.</span>
                         </h1>
                         </div>
                       </div>
                     </div>
                 </div>
             </div>
-        </card>
         `;
     }
 };
+
