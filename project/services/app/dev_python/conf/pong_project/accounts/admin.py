07a2f1454b1176a6ef836e5644279ee74a2f61b1
RootOfYggdrasil <simonedel94@gmail.com>
2024-10-13 09:50:31 +0200

    little things

--- project/services/app/dev_python/conf/pong_project/accounts/admin.py
+++ project/services/app/dev_python/conf/pong_project/accounts/admin.py
@@ -4,13 +4,13 @@ from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ['username', 'email', 'games_played', 'games_won_count', 'total_score', 'win_rate']
+    list_display = ['username', 'email', 'games_played', 'games_won_count', 'total_score', 'win_rate', 'friends']
     list_filter = UserAdmin.list_filter + ('games_played', 'games_won_count')
     fieldsets = UserAdmin.fieldsets + (
-        ('Game Statistics', {'fields': ('games_played', 'games_won_count', 'total_score')}),
+        ('Game Statistics', {'fields': ('games_played', 'games_won_count', 'total_score', 'friends')}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
-        ('Game Statistics', {'fields': ('games_played', 'games_won_count', 'total_score')}),
+        ('Game Statistics', {'fields': ('games_played', 'games_won_count', 'total_score', 'friends')}),
     )
 
 admin.site.register(CustomUser, CustomUserAdmin)
